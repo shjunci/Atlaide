4fcbe06621ef24df184b6510d938f29844ffe44f
Dmitrijs Sedovs <91872683+shjunci@users.noreply.github.com>
2021-12-06 08:56:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+def aprAtlaide(summa):
+  rez=""
+  if summa<100:
+    rez="Atlaide nav piešķirta, jānomaksā vēl" + str(summa)
+  elif summa>100 and summa <200:
+    rez="Atlaide 5%, jānomaksā vēl"  + str(summa*0.95)
+  else:
+    rez="Atlaide 10%, jānomaksā vēl"  + str(summa*0.9)
+  return rez
+
+summa=float(input("Ievadi summu:"))    
+print(aprAtlaide(summa))
